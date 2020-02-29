584986dd2de07da1c1896f68603a792fe19a06b5
btan <btan7645@gmail.com>
2020-02-28 19:15:16 -0500

    feat: Render pdf on page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+    return (
+      <div>
+        <Document
+          file="./example.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Page pageNumber={1}>
+          </Page>
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+        <h1>This is an example</h1>
+      </div>
+    );
+  }
 }
-
-export default App;
