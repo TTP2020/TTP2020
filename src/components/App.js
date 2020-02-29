3b423b57306c47168e16232e84d15973b577a30f
btan <btan7645@gmail.com>
2020-02-29 13:31:30 -0500

    feat: Change landing style

--- src/components/App.js
+++ src/components/App.js
@@ -21,10 +21,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Link to="/add">Add</Link>
-        <Link to="/resume">Resume</Link>
-        <Link to="/comments">Comments</Link>
-        <Link to="/">Landing</Link>
         <Routes />
       </div>
     );
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Container } from "semantic-ui-react";
+import { Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import MockDocument from "../MockDocument/MockDocument";
 import "./Landing.css";
@@ -18,26 +18,20 @@ export default class Landing extends Component {
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() { }
 
   render() {
     const { documents } = this.state;
     return (
       <div className="landing">
-        <Header
-          as="h1"
-          style={{
-            color: "white",
-            "font-family": "Montserrat",
-            "font-weight": 800,
-            "font-size": "150px"
-          }}
-        >
-          Welcome to Kritique
-        </Header>
-        <Header as="h2" style={{ color: "white" }}>
-          Pending Resumes
-        </Header>
+        <div>
+          <Image src="./logo.png" size="huge"></Image>
+        </div>
+        <div>
+          <h2 as="h2" style={{ color: "white" }}>
+            Pending Resumes
+        </h2>
+        </div>
         <div className="landing-documents">
           {documents.map(document => {
             return (
