58c9e56f3a9dd5de90301584e21f6accb5378d77
btan <btan7645@gmail.com>
2020-02-29 10:24:22 -0500

    feat: All pdf logic inside UserDocument component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { Page } from 'react-pdf';
-import { Document } from 'react-pdf/dist/entry.webpack';
-import { ReactComponent as Comment } from './assets/comment.svg';
+import UserDocument from './components/UserDocument'
 
 export default class App extends Component {
   constructor() {
@@ -17,80 +15,9 @@ export default class App extends Component {
     //TODO: Fetch markers from backend.
   }
 
-  handleClick = (x, y, comment) => {
-    let newMarker = {
-      x,
-      y,
-      pageNumber: this.state.pageNumber,
-      comments: [],
-    };
-    let key = `${x}${y}`;
-    console.log(this.state.markers);
-    this.setState(prevState => {
-      return {
-        markers: { ...prevState.markers, [key]: newMarker },
-      };
-    });
-  };
-
-  onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  };
-
-  goToPrevPage = () => {
-    const { pageNumber } = this.state;
-    if (pageNumber - 1 > 0) this.setState({ pageNumber: pageNumber - 1 });
-  };
-
-  goToNextPage = () => {
-    const { pageNumber, numPages } = this.state;
-    if (pageNumber + 1 <= numPages)
-      this.setState({ pageNumber: pageNumber + 1 });
-  };
-
   render() {
-    const { pageNumber, numPages, markers } = this.state;
-    const markersArr = Object.values(markers);
-    return (
-      <div className="App">
-        {markersArr.map(marker => {
-          const { x, y, pageNumber } = marker;
-
-          return this.state.pageNumber === pageNumber ? (
-            <Comment
-              key={`${x}${y}`}
-              style={{
-                width: '1%',
-                height: 'auto',
-                position: 'absolute',
-                color: 'red',
-                top: y - 15 + 'px',
-                left: x + 'px',
-                zIndex: 1,
-              }}
-            ></Comment>
-          ) : (
-            <></>
-          );
-        })}
-        <nav>
-          <button onClick={this.goToPrevPage}>Prev</button>
-          <button onClick={this.goToNextPage}>Next</button>
-          <p>
-            Page {pageNumber} of {numPages}
-          </p>
-        </nav>
-        <Document
-          onClick={event => this.handleClick(event.pageX, event.pageY)}
-          file="./example.pdf"
-          onLoadSuccess={this.onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber}></Page>
-        </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-      </div>
-    );
+    return <div class="App">
+      <UserDocument></UserDocument>
+    </div>
   }
 }
